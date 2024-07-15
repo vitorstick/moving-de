e3d46d02822e3767a96425132571c0d400092333
Vitor Ferreira <vitorstick@gmail.com>
2024-07-15 13:55:09 +0200

    cleanup after migrate

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom, isDevMode } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import {} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideServiceWorker } from '@angular/service-worker';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
